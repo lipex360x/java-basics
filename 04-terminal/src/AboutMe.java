bb1724a43fa47782622fcfbc3065c554b2e58a1c
Felipe Bambirra <lipex360@gmail.com>
2024-11-15 13:23:49 +0000

    class AboutMe with scanner added

--- 04-terminal/src/AboutMe.java
+++ 04-terminal/src/AboutMe.java
@@ -1,11 +1,20 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
   public static void main(String[] args) {
-    String name = args[0];
-    String lastName = args[1];
-    int age = Integer.valueOf(args[2]);
-    double height = Double.valueOf(args[3]);
-    System.out.println("Hello, my name is " + name + " " + lastName);
-    System.out.println("I am " + age);
-    System.out.println("My height is " + height + "cm");
+    try (Scanner scanner = new Scanner(System.in).useLocale(Locale.US)) {
+      System.out.println("Type your first name");
+      String firstName = scanner.next();
+      System.out.println("Type your last name");
+      String lastName = scanner.next();
+      System.out.println("Type your age");
+      int age = scanner.nextInt();
+      System.out.println("Type your height");
+      double height = scanner.nextDouble();
+      System.out.println("Hello, my name is " + firstName + " " + lastName);
+      System.out.println("I am " + age);
+      System.out.println("My height is " + height + "cm");
+    }
   }
 }
