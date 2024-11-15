587d13bb7cd3a0c4e60678ef21f42b98814d724a
Felipe Bambirra <lipex360@gmail.com>
2024-11-15 13:00:18 +0000

    documentation added

---
+++ 04-terminal/src/Calculator.java
@@ -0,0 +1,19 @@
+/**
+ * <h1>Calculator</h1>
+ * <p>Maths calculator</p>
+ * @author Felipe
+ * @version 1.0
+ * @since 01/01/2024
+ */
+
+public class Calculator {
+  /**
+   * Sum two numbers
+   * @param int num1 - first parameter
+   * @param int num2 - second parameter
+   * @return int result of sum 
+   */
+  public int sum(int num1, int num2) {
+    return num1 + num2;
+  }
+}
---
+++ docs/member-search-index.js
@@ -0,0 +1,1 @@
+memberSearchIndex = [{"p":"<Unnamed>","c":"App","l":"App()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"Calculator","l":"Calculator()","u":"%3Cinit%3E()"},{"p":"<Unnamed>","c":"App","l":"main(String[])","u":"main(java.lang.String[])"},{"p":"<Unnamed>","c":"Calculator","l":"sum(int, int)","u":"sum(int,int)"}];updateSearchResults();
