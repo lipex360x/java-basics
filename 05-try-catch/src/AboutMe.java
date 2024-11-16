a8a85395a3acd6331fba2c041ab679934eccec84
Felipe Bambirra <lipex360@gmail.com>
2024-11-16 21:17:46 +0000

    try/catch added

---
+++ 05-try-catch/src/AboutMe.java
@@ -0,0 +1,26 @@
+import java.util.InputMismatchException;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class AboutMe {
+  public static void main(String[] args) throws Exception {
+    try {
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        System.out.println("Por favor, digite o número da Agência");
+        String agency = scanner.next();
+        System.out.println("Agora, digite o numero da conta");
+        int accountNumber = scanner.nextInt();
+        System.out.println("Digite seu primeiro nome");
+        String firstName = scanner.next();
+        System.out.println("Digite seu primeiro nome");
+        String lastName = scanner.next();
+        System.out.println("Por fim, digite o valor do seu depósito");
+        double income = scanner.nextDouble();
+        String fullName = firstName.concat(" ").concat(lastName);
+        System.out.println("Olá " + fullName + " obrigado por criar uma conta em nosso banco, sua agência é " + agency + ", conta " + accountNumber + " e seu saldo " + income + " já está disponível para saque");
+        scanner.close();
+      } catch (InputMismatchException error) {
+        System.out.println("Invalid field");
+      }
+    }
+}
