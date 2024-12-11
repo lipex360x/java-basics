faf5ccc7de4c71601daa7347ba204fcaab06f8e5
Felipe Bambirra <lipex360@gmail.com>
2024-12-11 14:48:46 +0000

    nodes added

---
+++ 08-reference/src/com/project/reference/node/Node.java
@@ -0,0 +1,31 @@
+package com.project.reference.node;
+
+public class Node {
+  private String content;
+  private Node nextNode;
+
+  public Node(String content) {
+    this.content = content;
+    this.nextNode = null;
+  }
+
+  public String getContent() {
+    return this.content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+  public Node getNextNode() {
+    return this.nextNode;
+  }
+
+  public void setNextNode(Node nextNode) {
+    this.nextNode = nextNode;
+  }
+
+  public String toString() {
+    return this.content.toString();
+  }
+}
